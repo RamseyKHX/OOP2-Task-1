9f7fa18defebb6f9fc82b36b136d160a0364fdf8
RamseyKHX <rameez.ahmed@edu.sait.ca>
2024-01-21 19:01:06 -0700

    Add project files.

---
+++ Task 1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1 //
+
+            Console.WriteLine("Enter a low Number: ");
+            int lowNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a High Number: ");
+            int highNumber = Convert.ToInt32(Console.ReadLine());
+
+            int subtraction = highNumber - lowNumber;
+            Console.WriteLine("The Subtraction of " + highNumber + " and " + lowNumber + " is: " + subtraction);
+        }
+    }
+}
